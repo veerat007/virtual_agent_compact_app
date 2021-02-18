7e0aea4532be16321d7e34a4b20b9c29b311a02a
worawuttibphan <45648166+worawuttibphan@users.noreply.github.com>
2021-02-18 11:54:10 +0700

    add AmiCallResult

--- lib/application_helper.rb
+++ lib/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
 end
+module AmiCallResult
+    SUCCESS           = 'S'.freeze
+    TOO_MANY_ERRORS   = 'T'.freeze
+    EXIT              = 'X'.freeze
+    HANGUP            = 'H'.freeze
+    SYSTEM_ERROR      = 'E'.freeze
+  end
